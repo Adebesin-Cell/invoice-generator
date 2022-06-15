1d6a696b9cf1c4175591f376ad7943baab7c96a9
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-06-15 11:42:32 +0100

    updating files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import MobileMenu from "./components/mobileMenu/MobileMenu";
-import { createIcons, icons } from "lucide";
-import { useEffect } from "react";
-import MainPage from "./components/body/Body";
-
 function App() {
-  useEffect(() => {
-    createIcons({
-      icons,
-      "stroke-width": 1.5,
-      nameAttr: "data-lucide",
-    });
-    window.lucide = {
-      createIcons: createIcons,
-      icons: icons,
-    };
-  }, []);
-
-  return (
-    <>
-      <MobileMenu></MobileMenu>
-      <MainPage />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
