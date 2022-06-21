ab309eddd2f1a011bb70074dc603e4936e2598b5
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-06-21 09:23:10 +0100

    Updating UI

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,20 @@
+import Card from "../../utils/card/Card";
 import overlay from "../../assets/images/overlay.png";
 import styles from "./Dashboard.module.scss";
 import { ReactComponent as Help } from "../../assets/svgs/icon-help.svg";
+import { ReactComponent as Documentation } from "../../assets/svgs/icon-information.svg";
+
+const Externals = [
+  {
+    id: 1,
+    title: "Documentation",
+    link: "/",
+    icon: <Documentation />,
+    outerBg: "#f0f0ff",
+    innerBg: "#4945ff",
+    decription: "Discover the essential concepts, guides and instructions.",
+  },
+];
 
 const DashboardView = function (props) {
   return (
@@ -15,6 +29,38 @@ const DashboardView = function (props) {
       <div className={styles.view__container}>
         <div className={styles.view__intro}>
           <h1 className={styles.view__heading}>Welcome 👋</h1>
+          <p className={styles.view__paragraph}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+            temporibus laboriosam quas nisi dolorem ducimus, dolores pariatur
+            qui numquam, illo ullam porro recusandae deserunt vel.
+          </p>
+          <div className={styles.view__wrapper}>
+            <div className={styles.view__external}>
+              <ul className={styles.view__list}>
+                {Externals.map((external) => (
+                  <li key={external.id} className={styles.view__item}>
+                    <a href='/' className={styles.view__link}>
+                      <Card>
+                        <div className={styles.view__box}>
+                          <div
+                            className={styles.view__icon}
+                            style={{ backgroundColor: external.outerBg }}
+                          >
+                            <div
+                              className={styles["view__icon--box"]}
+                              style={{ backgroundColor: external.innerBg }}
+                            >
+                              {external.icon}
+                            </div>
+                          </div>
+                        </div>
+                      </Card>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
         <div className={styles.view__help}>
           <span className={styles["view__help--icon"]}>
