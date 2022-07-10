a2e471c044835b4dd6191863da3bdd909e5804aa
Adebesin Cell <adebesintolulope80@gmail.com>
2022-07-10 22:20:24 +0100

    Updating UI

--- src/components/create/product/NewProduct.js
+++ src/components/create/product/NewProduct.js
@@ -11,6 +11,7 @@ const NewProduct = function (props) {
   const fileUploadHandler = function (event) {
     setSelectedFile(event.target.files[0]);
     console.log(selectedFile);
+    //hello
   };
 
   return (
