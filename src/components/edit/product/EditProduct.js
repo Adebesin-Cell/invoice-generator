a99600c18858355c914afbc591a8fe32077b4da3
Adebesin Cell <adebesintolulope80@gmail.com>
2022-07-14 14:59:02 +0100

    Updating UI

--- src/components/edit/product/EditProduct.js
+++ src/components/edit/product/EditProduct.js
@@ -63,102 +63,112 @@ const EditProductComponent = function () {
             </p>
           </div>
         </div>
-        <form action='' className={styles.form}>
-          <div className={styles.form__group}>
-            <span className={styles.form__label}>Product Image</span>
-            <Input
-              id='product-image'
-              name='file'
-              type='file'
-              accept='image/*'
-              className={styles.form__upload}
-              onChange={fileUploadHandler}
-            />
-            <label htmlFor='product-image' className={styles.form__customLabel}>
-              <div className={styles.form__icon}>
-                <Camera />
-              </div>
-              <p className={styles.form__text}>
-                {selectedFile ? selectedFile.name : "Add new product image"}
-              </p>
-            </label>
-          </div>
-          <div className={styles.form__group}>
-            <label htmlFor='product-name' className={styles.form__label}>
-              Product Name
-            </label>
-            <Input
-              id='product-name'
-              name='product-name'
-              type='text'
-              className={styles.form__input}
-              placeholder='Product Name'
-            />
-          </div>
-          <div className={styles.form__group}>
-            <label htmlFor='product-name' className={styles.form__label}>
-              Product Price (NGN)
-            </label>
-            <Input
-              id='product-price'
-              name='product-price'
-              type='number'
-              className={styles.form__input}
-              placeholder='Product price'
-            />
-          </div>
-          <div className={styles.form__group}>
-            <label htmlFor='product-quantity' className={styles.form__label}>
-              Quantity Available
-            </label>
-            <Input
-              id='product-quantity'
-              name='product-quantity'
-              type='number'
-              className={styles.form__input}
-              placeholder='Quantity Available'
-              step='1'
-              min='1'
-            />
-          </div>
-          <div className={styles.form__group}>
-            <label htmlFor='product-category' className={styles.form__label}>
-              Product Category
-            </label>
-            <Input
-              id='product-category'
-              name='product-category'
-              type='text'
-              className={styles.form__input}
-              placeholder='Product Category'
-            />
-          </div>
-          <div className={styles.form__group}>
-            <label htmlFor='discount' className={styles.form__label}>
-              Discount
-            </label>
-            <Input
-              id='discount'
-              name='product-discount'
-              type='number'
-              className={styles.form__input}
-              placeholder='Discount'
-            />
-          </div>
-          <div className={`${styles.form__group} ${styles["form__group--lg"]}`}>
-            <label htmlFor='product-description' className={styles.form__label}>
-              Product Description
-            </label>
-            <textarea
-              name='product-description'
-              id='product-description'
-              cols='30'
-              rows='10'
-              className={styles.form__textarea}
-              placeholder='Product Description'
-            ></textarea>
-          </div>
-        </form>
+        <div className={styles.view__form}>
+          <form action='' className={styles.form}>
+            <div className={styles.form__group}>
+              <span className={styles.form__label}>Product Image</span>
+              <Input
+                id='product-image'
+                name='file'
+                type='file'
+                accept='image/*'
+                className={styles.form__upload}
+                onChange={fileUploadHandler}
+              />
+              <label
+                htmlFor='product-image'
+                className={styles.form__customLabel}
+              >
+                <div className={styles.form__icon}>
+                  <Camera />
+                </div>
+                <p className={styles.form__text}>
+                  {selectedFile ? selectedFile.name : "Add new product image"}
+                </p>
+              </label>
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor='product-name' className={styles.form__label}>
+                Product Name
+              </label>
+              <Input
+                id='product-name'
+                name='product-name'
+                type='text'
+                className={styles.form__input}
+                placeholder='Product Name'
+              />
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor='product-name' className={styles.form__label}>
+                Product Price (NGN)
+              </label>
+              <Input
+                id='product-price'
+                name='product-price'
+                type='number'
+                className={styles.form__input}
+                placeholder='Product price'
+              />
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor='product-quantity' className={styles.form__label}>
+                Quantity Available
+              </label>
+              <Input
+                id='product-quantity'
+                name='product-quantity'
+                type='number'
+                className={styles.form__input}
+                placeholder='Quantity Available'
+                step='1'
+                min='1'
+              />
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor='product-category' className={styles.form__label}>
+                Product Category
+              </label>
+              <Input
+                id='product-category'
+                name='product-category'
+                type='text'
+                className={styles.form__input}
+                placeholder='Product Category'
+              />
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor='discount' className={styles.form__label}>
+                Discount
+              </label>
+              <Input
+                id='discount'
+                name='product-discount'
+                type='number'
+                className={styles.form__input}
+                placeholder='Discount'
+              />
+            </div>
+            <div
+              className={`${styles.form__group} ${styles["form__group--lg"]}`}
+            >
+              <label
+                htmlFor='product-description'
+                className={styles.form__label}
+              >
+                Product Description
+              </label>
+              <textarea
+                name='product-description'
+                id='product-description'
+                cols='30'
+                rows='10'
+                className={styles.form__textarea}
+                placeholder='Product Description'
+              ></textarea>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
