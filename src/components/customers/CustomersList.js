590aa802036b909ae93ee4c5dbb8cbe04768b324
Adebesin Cell <adebesintolulope80@gmail.com>
2022-08-25 14:13:44 +0100

    Updating UI

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -4,6 +4,9 @@ import Button from '../UI/button/Button';
 import * as Icon from 'react-feather';
 import Modal from '../modal/Modal';
 import { useState, useEffect } from 'react';
+import { RecentSales } from '../../data/Data';
+import { FormatDate } from '../../utils/formatDate/FormatDate';
+import { FormatNumber } from '../../utils/formatNumber/FormatNumber';
 
 const CustomersList = function () {
   const [body, setBody] = useState('');
@@ -39,22 +42,46 @@ const CustomersList = function () {
                 <thead>
                   <tr>
                     <th>S/N</th>
-                    <th>Invoice No.</th>
-                    <th>Customer</th>
+                    <th>Full Name</th>
                     <th>Customer Phone</th>
-                    <th>Date of Invoice</th>
-                    <th>Total Amount</th>
+                    <th>Email</th>
+                    <th>City</th>
+                    <th>Last Transaction</th>
                     <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr onClick={(e) => showModalHandler(e)}>
                     <td>#1</td>
-                    <td>2344</td>
-                    <td>Tolulope Adebesin</td>
+                    <td>Tolulope Ade</td>
                     <td>08124333444</td>
-                    <td>13, June, 2020</td>
-                    <td>99000</td>
+                    <td>tolu@gmail.com</td>
+                    <td>Lagos</td>
+                    <td>#29292</td>
+                    <td className={styles.customers__tableAction}>
+                      <Button
+                        className={styles.customers__tableEdit}
+                        title='edit'
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        <Icon.Edit />
+                      </Button>
+                      <Button
+                        className={styles.customers__tableDelete}
+                        title='delete'
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        <Icon.Trash2 />
+                      </Button>
+                    </td>
+                  </tr>
+                  <tr onClick={(e) => showModalHandler(e)}>
+                    <td>#1</td>
+                    <td>Tolulope Ade</td>
+                    <td>08124333444</td>
+                    <td>tolu@gmail.com</td>
+                    <td>Lagos</td>
+                    <td>#29292</td>
                     <td className={styles.customers__tableAction}>
                       <Button
                         className={styles.customers__tableEdit}
@@ -106,107 +133,70 @@ const CustomersList = function () {
                   <span className={styles.customer__key}>City:</span>
                   <span className={styles.customer__value}>Lagos</span>
                 </li>
+                <li className={styles.customer__item}>
+                  <span className={styles.customer__key}>Address 1:</span>
+                  <span className={styles.customer__value}>&nbsp;</span>
+                </li>
+                <li className={styles.customer__item}>
+                  <span className={styles.customer__key}>Address 2:</span>
+                  <span className={styles.customer__value}>&nbsp;</span>
+                </li>
               </ul>
               <h1 className={styles.customer__heading}>
-                <span className={styles.customer__text}>Invoice Details</span>
-                <span className={styles.customer__icon}>
-                  <Icon.Check />
+                <span className={styles.customer__text}>
+                  Transaction Summary
                 </span>
               </h1>
-              <ul className={styles.customer__list}>
-                <li className={styles.customer__item}>
-                  <span className={styles.customer__key}>ID</span>
-                  <span className={styles.customer__value}>#123939</span>
-                </li>
-                <li className={styles.customer__item}>
-                  <span className={styles.customer__key}>Date:</span>
-                  <span className={styles.customer__value}>4/8/22</span>
-                </li>
-                <li className={styles.customer__item}>
-                  <span className={styles.customer__key}>Items:</span>
-                </li>
+              <ul className={styles.transaction__list}>
+                {RecentSales.map((product) => (
+                  <li className={styles.transaction__item}>
+                    <span
+                      className={`${styles.transaction__icon} ${
+                        product.hasPaid
+                          ? styles['transaction__icon--success']
+                          : styles['transaction__icon--failed']
+                      }`}
+                    >
+                      <Icon.Check />
+                    </span>
+                    <div className={styles.transaction__info}>
+                      <h1 className={styles.transaction__title}>
+                        <span className={styles.transaction__id}>
+                          {product.sales_id}
+                        </span>
+                        <span className={styles.transaction__date}>
+                          <FormatDate
+                            locale='en-US'
+                            date={product.sales_date}
+                          />
+                        </span>
+                      </h1>
+                      <h1 className={styles.transaction__name}>
+                        {product.sales_by}
+                      </h1>
+                    </div>
+                    <div className={styles.transaction__details}>
+                      <h1 className={styles.transaction__price}>
+                        <FormatNumber
+                          type='currency'
+                          amount={product.sales_cost}
+                          currency={product.currency}
+                          locale='en-US'
+                        />
+                      </h1>
+                      <p
+                        className={`${styles.transaction__progress} ${
+                          product.hasPaid
+                            ? styles['transaction__paid']
+                            : styles['transaction__not-paid']
+                        }`}
+                      >
+                        {product.hasPaid ? 'Paid' : 'Not Paid'}
+                      </p>
+                    </div>
+                  </li>
+                ))}
               </ul>
-              <table className={styles.customer__table}>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                  </tr>
-                </thead>
-                <tbody className={styles.customer__tableBody}>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                  <tr>
-                    <td>Shoes</td>
-                    <td>3</td>
-                    <td>3,000</td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
           </div>
         </Modal>
